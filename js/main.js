d2dd390f933c0a2e91e5b88b45d23a95934bb19c
Алиса <amoldashev@gmail.com>
2021-06-02 23:22:45 +0500

    Исправляет функции после ESLint теста и устанавливает npm

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
 // Первое задание. Получение целого числа + условия.
-function getRandomInt (min, max) {
-  let random = Math.floor(Math.random() * (max - min)) + min;
+const ERROR_MESSAGE = new Error('Невозможно разобрать входные данные');
 
-  if (random >= 0, max >= min) {
+function getRandomInt (min, max) {
+  const RANDOM = Math.floor(Math.random() * (max - min)) + min;
+
+  if (RANDOM >= 0, max >= min) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    console.log('Получено случайное значение: ' + random.toString());
-  } else {
-    console.log('Получено отрицательное значение! ' + random.toString());
-  };
+  }
 
-  if (max < min ) {
-    console.log('Используется недопустимый диапазон значений');
-  };
-};
+  if (max < min) {
+    throw ERROR_MESSAGE;
+  }
+}
 
 getRandomInt(-2, -5);
 
-Источник: [MDN] ('https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random')
+// Источник: [MDN] ('https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random')
 
 
 // Второе задание!
 
-let textValue = 'Строка - но не сто сорок символов';
-let lengthNumber = textValue.length;
-let maxLength = 140;
+const TEXT_VALUE = 'Строка - но не сто сорок символов';
+const LENGTH_NUMBER = TEXT_VALUE.length;
 
-function getLength (textValue, maxLength) {
-  if (lengthNumber <= 140) {
-    console.log('Число не превышает 140 симоволов ' + lengthNumber.toString());
+function getLength (inspectedLine, maxLength) {
+  if (LENGTH_NUMBER <= maxLength) {
     return true;
   } else {
-      console.log('Число вводимых символов превышает максимальное значение! ' + lengthNumber.toString());
-      return false;
-    }
+    return false;
+  }
 }
 
 getLength();
