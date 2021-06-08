29bae2cbb38c0339f055d3844cef1c21a2c46362
Алиса <amoldashev@gmail.com>
2021-06-08 08:02:03 +0500

    Исправляет путь подключения js файла, правит js файл

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // AN ARRAY with 25 generated objects. Each array contains a photocard;
-let Gallery = new Array (25);
+let GalleryArray = new Array (25);
 // THE PHOTOCARD shall include the following:
 let photocardArray = {
   id: photocardId,
   url: urlPath,
   description: SomeTextArray,
-  likes: ,
-  comments: ,
+  likes: photoLikes,
+  comments: commentsArray,
 }
 
 // EACH ITEM IS AN ARRAY!
@@ -14,11 +14,15 @@ let photocardArray = {
 
 // ID. numeric; unique, min = 1; max = 25;
 // - id;
-let photocardId;
 
+let isCycleFunction = () => {
   for (let i = 1; i <= 25; i++) {
-    return photocardId = i;
+    let IdNumber = i;
+  }
 }
+console.log(IdNumber.toString());
+let photocardId = isCycleFunction();
+isCycleFunction();
 
 // URL. img; src 'i.jpg'; let i; min = 1; max = 25;
 // - url;
@@ -31,43 +35,11 @@ let urlPath;
 
 // DESCRIPTION. text; generated.
 // - description;
-let photoDescription = 'Кекстаграммная котейка, лижет кекстаорехи';
+let photoDescription = 'Кекстаграммная котейка';
 
 // LIKES. numeric; random; min = 15 max = 200.
 // - likes;
-let likes;
-
-// COMMENTS. text; Array; comment list; amount = random;
-// - comments;
-let comments;
-
-let comentsArray = {
-  id: 135,
-  avatar: 'img/avatar-6.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: 'Артём',
-}
-
-// COMMENT ID. numeric; random; unique;
-let commentsId;
-
-// COMMENT avatar. string; value min = 1 max = 6 i.e. avatar-6.svg;
-// - avatar field;
-
-// COMMENT MESSAGE. one or two sentences to be included as an examp
-
-// Всё отлично!
-// В целом всё неплохо. Но не всё.
-// Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-// Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-// Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-// Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
-
-// СOMMENT NAMES. text; random; Optional;
-
-
-
-/* // Первое задание. Получение целого числа + условия.
+let photoLikes = getRandomInt(15, 200);
 const ERROR_MESSAGE = new Error('Невозможно разобрать входные данные');
 
 function getRandomInt(min, max) {
@@ -83,10 +55,76 @@ function getRandomInt(min, max) {
   }
 }
 
-getRandomInt(-2, -5);
 
-// Источник: [MDN] ('https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random')
 
+// COMMENTS. text; Array; comment list; amount = random;
+// - comments;
+// let comments;
+//
+// let commentsArray = {
+//   id: 135,
+//   avatar: 'img/avatar-6.svg',
+//   message: 'В целом всё неплохо. Но не всё.',
+//   name: commentNames,
+// }
+
+// COMMENT ID. numeric; random; unique;
+let commentsId;
+let RandomIdArray = new Array(25);
+
+
+let getId = function () {
+  for (let i = 1; i <= 25; i++) {
+    let currentRandomNumber = getRandomInt(1, 100);
+    RandomIdArray.push(currentRandomNumber);
+  }
+}
+
+getId();
+console.log('RandomIdArray.toString()');
+
+
+// const TEXT_VALUE = 'Строка - но не сто сорок символов';
+// const LENGTH_NUMBER = TEXT_VALUE.length;
+//
+// function getLength(inspectedLine, maxLength) {
+//   if (LENGTH_NUMBER <= maxLength) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// getLength();
+
+
+// COMMENT avatar. string; value min = 1 max = 6 i.e. avatar-6.svg;
+// - avatar field;
+
+// COMMENT MESSAGE. one or two sentences to be included as an examp
+
+// Всё отлично!
+// В целом всё неплохо. Но не всё.
+// Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
+// Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
+// Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
+// Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
+
+// СOMMENT NAMES. text; random; Optional;
+commentNames
+let FirstName = {
+  title: 'Антуан',
+  title: 'Сильвуплэ',
+  title: 'Рикки',
+  title: 'Максимильян',
+}
+
+let SurNames = {
+  SurName: 'Дзюба',
+  SurName: 'Рошан',
+  SurName: 'Шекспиров',
+  SurName: 'Дебильный',
+}
 
 // Второе задание!
 
@@ -101,4 +139,4 @@ function getLength(inspectedLine, maxLength) {
   }
 }
 
-getLength(); */
+getLength();
