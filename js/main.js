5ab2a0fc8872ac29072e8dd95b9dfa173ead84d8
Алиса <amoldashev@gmail.com>
2021-06-09 23:13:55 +0500

    Вносит правки по ДЗ

--- js/main.js
+++ js/main.js
@@ -1,168 +1,103 @@
-/*
+const ERROR_MESSAGE = new Error('Bad Number!');
+const MAX_COMMENT_COUNT = 6;
+const MAX_MESSAGE_COUNT = 6;
+const NUMBER_ARRAYS = [];
 
-// AN ARRAY with 25 generated objects. Each array contains a photocard;
-    let galleryArray = new Array (25);
-    // THE PHOTOCARD shall include the following:
-    let photocardArray = {
-      id: NumberArrays,
-      url: newStr,
-      description: SomeTextArray,
-      likes: photoLikes,
-      comments: commentsArray,
-    }
+const MAX_LIKE = 2000;
+const MIN_LIKE = 0;
+const RANDOM_LIKE = 0;
+const DESCRIPTION_INDEX = 0;
+const MESSAGE_INDEX = 0;
+const MAX_PHOTOCARD_AMOUNT = 25;
 
- */
-// EACH ITEM IS AN ARRAY!
-// EACH ARRAY HAS REQUIREMENTS!
+const MESSAGE_ARRAYS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
-// ID. numeric; unique, min = 1; max = 25;
-// - id;
-let NumberArrays = []
+const QUOTES_ARRAYS = [
+  'Кекстаграммная котейка',
+  'Блудливый котян',
+  'Ночной пуритан',
+  'Предсказуемый кот-жрец',
+  'Развратный жмурик',
+];
 
-  for (let i = 1; i <= 25; i++) {
-    idNumber = i;
-    NumberArrays.push(idNumber);
-  }
+const NAMES_ARRAYS = [
+  'Антуан',
+  'Роджер',
+  'Рошан',
+  'Максимильян',
+  'Хачатурян',
+  'Вазовски',
+];
 
-// function startFromZero () {
-//   for (let i = 0; i < 25; i++) {
-//     idNumber = i;
-//   }
-// }
+// Вызывает случайную цитату
 
-
-// URL. img; src 'i.jpg'; let i; min = 1; max = 25;
-// let urlPath = 'photos/${{i}}.jpg';
-let urlNumber;
-
-
-// function replaceString () {
-//   let RegExp = '${{i}}';
-//   newStr = urlPath.replace(RegExp, urlNumberFunction);
-//   // for (let i = 1; i <= 25; i++) {urlNumber = i;}
-//   return newStr;
-// }
-// replaceString();
-// console.log(newStr)
-// replaceString();
-// console.log(newStr)
-
-let = urlNumberFunction = function getUrlArray () {
-  let emptyArray = NumberArrays.slice();
-  urlNumber = 0;
-  emptyArray.forEach(element => {
-    urlNumber = emptyArray[element - 2];
-    console.log(urlNumber)
-    return urlNumber;
-  });
-
+function getRandomQuote() {
+  DESCRIPTION_INDEX = Math.floor(Math.random()*QUOTES_ARRAYS.length);
+  return QUOTES_ARRAYS[DESCRIPTION_INDEX];
 }
-getUrlArray();
-console.log(urlNumberFunction)
-// DESCRIPTION. text; generated.
-// - description;
-// let quotesArray = [
-//   'Кекстаграммная котейка',
-//   'Блудливый котян',
-//   'Ночной пуритан',
-//   'Предсказуемый кот-жрец',
-//   'Развратный жмурик',
-// ]
 
-// let descriptionIndex;
-// let getRandomQuote = function () {
-//   descriptionIndex = Math.floor(Math.random()*quotesArray.length);
-//   quotesArray[descriptionIndex];
-// }
-// getRandomQuote();
-// console.log(quotesArray[descriptionIndex]);
+// Выставляет случайное число лайков
 
-// LIKES. numeric; random; min = 15 max = 200.
-// - likes;
-// let photoLikes;
-const ERROR_MESSAGE = new Error('Невозможно разобрать входные данные');
-let randomNumber;
-function getRandomInt(min, max) {
-  randomNumber = Math.floor(Math.random() * (max - min)) + min;
+function getRandomLike(MIN_LIKE, MAX_LIKE) {
+  RANDOM_LIKE = Math.floor(Math.random() * (MAX_LIKE - MIN_LIKE)) + MIN_LIKE;
 
-  if (randomNumber >= 0, max >= min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  }
-
-  if (max < min) {
+  if (RANDOM_LIKE >= 0, MAX_LIKE >= MIN_LIKE) {
+    MIN_LIKE = Math.ceil(MIN_LIKE);
+    MAX_LIKE = Math.floor(MAX_LIKE);
+  } else {
     throw ERROR_MESSAGE;
   }
-  return randomNumber;
+  return RANDOM_LIKE;
 }
-// photoLikes = RANDOM;
-// getRandomInt(15, 200);
+
+// Вызывает случайное сообщение
+
+function createRandomMessage(MAX_MESSAGE_COUNT) {
+  let randomMessageArrays = [];
+  for (let messageCount = 0; messageCount < MAX_MESSAGE_COUNT; messageCount++) {
+    let messageIndex = Math.floor(Math.random()*messageArrays.length);
+    randomMessageArrays.push(messageArrays[messageIndex])
+    return randomMessageArrays.toString();
+  }
+}
+
+// Добавляет случайное имя
+
 
 
 // COMMENTS. text; Array; comment list; amount = random;
 // - comments;
-// let comments;
-
-// let commentsArray = {
-//   id: ,
-//   avatar: 'img/avatar-6.svg',
-//   message: 'В целом всё неплохо. Но не всё.',
-//   name: commentNames,
-// }
-
-// COMMENT ID. numeric; random; unique;
-// let randomIdArray = [];
-// let currentRandomNumber;
-
-// for (let j = 1; j <= 25; j++) {
-//   currentRandomNumber = getRandomInt(1, 1000);
-//   randomIdArray.push(currentRandomNumber);
-// }
-
-
-// COMMENT avatar. string; value min = 1 max = 6 i.e. avatar-6.svg;
-// - avatar field;
-
-let formAvatarLink = function (item, index) {
-  
+function createCommentsBlock (MAX_COMMENT_COUNT) {
+  let commentsArrays = [];
+  for (let commentsIdNumber = 1; commentsIdNumber <= MAX_COMMENT_COUNT; commentsIdNumber++) {
+    commentsArrays.push ({
+      id: commentsIdNumber,
+      avatar: `img/avatar-${{commentsIdNumber}}.svg`,
+      message: createRandomMessage(),
+      name: commentNames,
+    });
+  };
 }
 
-// COMMENT MESSAGE. one or two sentences to be included as an examp
+// Создает блок карточек
 
-// Всё отлично!
-// В целом всё неплохо. Но не всё.
-// Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-// Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-// Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-// Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
-
-// СOMMENT NAMES. text; random; Optional;
-// commentNames
-// let FirstName = {
-//   title: 'Антуан',
-//   title: 'Сильвуплэ',
-//   title: 'Рикки',
-//   title: 'Максимильян',
-// }
-
-// let SurNames = {
-//   SurName: 'Дзюба',
-//   SurName: 'Рошан',
-//   SurName: 'Шекспиров',
-//   SurName: 'Дебильный',
-// }
-
-// // Второе задание!
-
-// const TEXT_VALUE = 'Строка - но не сто сорок символов';
-// const LENGTH_NUMBER = TEXT_VALUE.length;
-
-// function getLength(inspectedLine, maxLength) {
-//   if (LENGTH_NUMBER <= maxLength) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// getLength();
+function setPhotocardData(MAX_PHOTOCARD_AMOUNT) {
+  const PHOTOCARD_ARRAY = [];
+  for (let idNumber = 1; idNumber <= MAX_PHOTOCARD_AMOUNT; idNumber++) {
+    PHOTOCARD_ARRAY.push ({
+      id: idNumber,
+      url: `photos/${{idNumber}}.jpg`,
+      description: getRandomQuote(),
+      likes: getRandomLike(MIN_LIKE, MAX_LIKE),
+      comments: createCommentsBlock(MAX_COMMENT_COUNT),
+    });
+  }
+}
+setPhotocardData(MAX_PHOTOCARD_AMOUNT);
