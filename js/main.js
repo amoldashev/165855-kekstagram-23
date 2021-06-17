e8299c1423eab2953bbccf1dc3f0eec2edc2703f
Azamat M <amoldashev@gmail.com>
2021-06-17 14:32:31 +0500

    Удаляет лишний импорт

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { setPhotocard } from './set-photocard.js';
 import { MAX_PHOTOCARD_AMOUNT } from './data.js';
 import './create-comment.js';
-import './get-random-number.js';
 
 setPhotocard(MAX_PHOTOCARD_AMOUNT);
