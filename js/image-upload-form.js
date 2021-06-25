e57f682f260ad036e0defc21913f7315a19b5bed
Azamat M <amoldashev@gmail.com>
2021-06-25 17:49:22 +0500

    Вносит правки

--- js/image-upload-form.js
+++ js/image-upload-form.js
@@ -74,8 +74,7 @@ uploadCancel.addEventListener(
   },
 );
 
-
-// 6. TODO:Напишите код для валидации формы добавления изображения. Список полей для валидации:
+// 6. TODO: Напишите код для валидации формы добавления изображения. Список полей для валидации:
 // Хэш-теги
 const inputTextHashtags = uploadSelectImage.querySelector('input.text__hashtags');
 const inputTextDescription = uploadSelectImage.querySelector('.text__description');
@@ -83,45 +82,63 @@ const inputTextDescription = uploadSelectImage.querySelector('.text__description
 const MIN_HASHTAG_LENGTH = 2;
 const MAX_HASHTAG_LENGTH = 20;
 
-// var str = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя';
-// var regexp = /[А-Д]/gi;
-// var matches_array = str.match(regexp);
-// console.log(matches_array);
+// TODO:Regular Expressions to be used
+const HashtagExp = new RegExp('#[A-Za-zА-Яа-яЁё0-9]{1,19}', 'gi');
+const symbolsExp = new RegExp('\W+', 'gi');
 
-// RegEx
-// \D - числа [^0-9]
-// [\s] - whitespace
-// [[:punct:]] - punctuation
-// [\W] symbols
-// [pattern, flags] flag - i => игнорирование регистра
+const isRegExp = (input, RegExp) => input.value !== RegExp; // Compares str with a reg expression
+const RegExpArr = []; // Creates empty array
+const matchExp = (input)  => input.value.match(RegExp);
 
-// new RegExp('', 'i')
+// FIXME:Сравнить строку с регулярным выражением.
+// const InputArr = [];
+// currentIndex =
+const fillInputArr = (input) => {
+  for (let i = 0; i <= input.value.length; i++) {
+    console.log(input.value.slice(i, 1));
+  }
+};
+
+
+
+fillInputArr(inputTextHashtags);
+// Вернуть сообщение о спецсимволах
+
+// const takeResultsFromExp = (input, RegExp, arr) => {
+//   RegExpArr.push(input.value);
+//   if(input.value.length >= 1) {
+//     arr.forEach(call(_, 1, RegExpArr) {matchExp(inputTextHashtags, RegExp)});
+//   }
+//   return arr;
+// };
+
+// console.log(RegExpArr);
+// const compareExp = (input, RegExp) => {
+// }
 
 // TODO:Проверка наличия хештега
 inputTextHashtags.addEventListener(
   'input',
   () => {
     const valueLength = inputTextHashtags.value.length;
-    if (valueLength < MIN_HASHTAG_LENGTH) {
+    if (inputTextHashtags.value.substr(0, 1) !== '#') {
+      inputTextHashtags.setCustomValidity('Начните вводить хэштег со знака решетка');
+    } else if (inputTextHashtags.value.length >= 1) { // Вернуть истину для регВыражения
+      takeResultsFromExp(inputTextHashtags, HashtagExp);
+      if (RegExpArr.length > 0) {
+        inputTextHashtags.setCustomValidity('Contains special symbols');
+      }
+      inputTextHashtags.setCustomValidity('Спец. символы не допустимы');
+    } else if (valueLength < MIN_HASHTAG_LENGTH) {
       inputTextHashtags.setCustomValidity(`Еще ${MIN_HASHTAG_LENGTH - valueLength} симв.`);
     } else if (valueLength > MAX_HASHTAG_LENGTH) {
       inputTextHashtags.setCustomValidity(`Удалите еще ${valueLength - MAX_HASHTAG_LENGTH} симв.`);
     } else {
       inputTextHashtags.setCustomValidity('');
     }
+    inputTextHashtags.reportValidity();
   },
-  // () => {
-  //   const valueLength = inputTextHashtags.value.length;
 
-  //   if (valueLength < MIN_HASHTAG_LENGTH) {
-  //     inputTextHashtags.setCustomValidity(`Еще ${MIN_HASHTAG_LENGTH - valueLength} символ.`);
-  //   } else if (valueLength > MAX_HASHTAG_LENGTH) {
-  //     inputTextHashtags.setCustomValidity(`Удалите еще ${valueLength - MAX_HASHTAG_LENGTH} симв.`);
-  //   } else {
-  //     inputTextHashtags.setCustomValidity('');
-  //   }
-  //   inputTextHashtags.reportValidity();
-  // },
   // () => {
   //   const expression = /^#[A-Za-zА-Яа-я0-9]{1,19}$/; // Пытаюсь использовать regexp
   //   if (expression.test(inputTextHashtags.value)) {
@@ -130,26 +147,6 @@ inputTextHashtags.addEventListener(
   //   return false;
   // },
 );
-
-// function validateFirstLetter(input) {
-//   let re = /^[#].*/;
-//   return re.test(input);
-// }
-// validateFirstLetter(inputTextHashtags);
-// const isHashtag = () =>
-
-
-// if (isHashtag) {
-
-// }
-// function validateUploadForm(evt) {
-//   evt.preventDefault();
-// }
-
-// UploadSelectImage.addEventListener(
-//   'submit',
-//   validateUploadForm,
-// );
 
 // function customValidation() {};
 
@@ -171,6 +168,31 @@ inputTextHashtags.addEventListener(
 //   }
 // }
 
+// var str = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя';
+// var regexp = /[А-Д]/gi;
+// var matches_array = str.match(regexp);
+// console.log(matches_array);
+
+// RegEx
+// \D - числа [^0-9]
+// [\s] - whitespace
+// [[:punct:]] - punctuation
+// [\W] symbols
+// [pattern, flags] flag - i => игнорирование регистра
+
+// new RegExp('', 'i')
+// const objPhrases = {
+//   firstLetterIsHash: 'Хеш-тег начинается с символа \'#\' (решётка)',
+//   specSymbols: 'Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т.п.), cимволы пунктуации (тире, дефис запятая и т.п.), эмодзи и т.д.',
+//   singleHash: 'Хеш-тег не может состоять только из одной решётки',
+//   maxLength: 'Максимальная длина одного хэш-тега 20 символов, включая решётку',
+//   register: 'Хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом',
+//   spaceValidation: 'Хэш-теги разделяются пробелами',
+//   unique: 'Один и тот же хэш-тег не может быть использован дважды',
+//   quantity: 'нельзя указать больше пяти хэш-тегов',
+//   required: 'хэш-теги необязательны',
+//   isEsc: 'Если фокус находится в поле ввода хэш-тега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения',
+// };
 // Комментарий
 
 // 7. TODO:Реализуйте логику проверки так, чтобы, как минимум, она срабатывала при попытке отправить форму
@@ -183,18 +205,3 @@ inputTextHashtags.addEventListener(
 
 // Валидация хеш-тегов?
 // Поля, не перечисленные в техзадании, но существующие в разметке, особой валидации не требуют.
-
-
-//   let errors = [];
-
-// if (inputHashtags.value == "") {
-//   errors.push({text: "Хэштэг", el: inputHashtags});
-// }
-
-// if (inputTextDescription.value == "") {
-//   errors.push({text: "Текстовое поле", el: inputTextDescription});
-// }
-// if (errors.length > 0) {
-//   alert('ERRORS');
-//   return false;
-// }
