b030540fdcef723149fe6fe6a4304c7a8c4251f5
Azamat M <amoldashev@gmail.com>
2021-06-30 17:44:04 +0500

    Вносит правки

--- js/validation.js
+++ js/validation.js
@@ -1,68 +1,94 @@
 
 // 6. TODO: Напишите код для валидации формы добавления изображения. Список полей для валидации:
+
+const { get } = require("browser-sync");
+
 // Хэш-теги
 const inputTextHashtags = document.querySelector('.text__hashtags');
+// const formUpload = document.querySelector('#upload-select-image');
 // const inputTextDescription = uploadSelectImage.querySelector('.text__description');
 
 const MIN_HASHTAG_LENGTH = 2;
 const MAX_HASHTAG_LENGTH = 20;
 
 // // TODO:Regular Expressions to be used
-const HashtagExp = new RegExp('#[A-Za-zА-Яа-яЁё0-9]{1,19}', 'gi');
+const HashtagExp = new RegExp('^#[A-Za-zА-Яа-яЁё0-9]{1,19}$', 'gi');
 const symbolsExp = new RegExp('\W+', 'gi');
-
-// const getMatchResults = (input, RegExp) => input.value.match(RegExp); // Compares str with a reg expression
-// console.log(getMatchResults(inputTextHashtags))
-
-const RegExpArr = []; // Creates empty array for incoming data
-// const matchExp = (arr)  => arr.match(RegExp);
 
 // FIXME:Сравнить строку с регулярным выражением.
 
-const fillInputArr = (input) => {
-  const InputArr = new Array();
-  let results;
-  for (let i = 0; i <= input.value.length; i++) {
-    InputArr.push(input.value.slice(i, 1)); //
-    results = InputArr.toString().match(new RegExp('\W+', 'gi'));
-    // console.log(results);
-  }
-  // if (!results <= 0) {
-  //   input.setCustomValidity('Строка пустая');
-  // }
+// var str = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя';
+// var regexp = /[А-Д]/gi;
+// var matches_array = str.match(regexp);
+// console.log(matches_array);
+
+// RegEx
+// \D - числа [^0-9]
+// [\s] - whitespace
+// [[:punct:]] - punctuation
+// [\W] symbols
+// [pattern, flags] flag - i => игнорирование регистра
+const validity = {
+  message: 'Cтрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т.д.'
 };
 
-// fillInputArr(inputTextHashtags);
-// Вернуть сообщение о спецсимволах
-// const takeResultsFromExp = (input, RegExp, arr) => {
-//   arr.push(input.value);
-//   if(input.value.length >= 1) {
-//     arr.forEach(matchExp(inputTextHashtags, RegExp));
-//   }
-//   return RegExpArr;
-// };
+const isHashtagFirst = (input) => input.value.substr(0, 1) === '#';
+const valueLength = (input) => input.value.length;
 
+const pushUniqueHashtags = (input) => {
+  const uniqueSet = new Set();
+  return uniqueSet.add(input.value.toLowerCase().trim().split(' ').filter((hashtag) => hashtag)); // Взять хэштег из инпута
+};
+
+const isSpaceAfterHash = (input) => input.value.test(/#+\s/);
+const isHashAfterSpace = (input) => input.value.test(/\s+#/);
+
+const validateHashtag = (input) => {
+  if (input.value !== '') {
+    const hashtags = pushUniqueHashtags(input);
+    hashtags.forEach((hashtag) => {
+      if (!HashtagExp.test(hashtag)) {
+        input.setCustomValidity(validity.message);
+        input.classList.add('invalid');
+        evt.preventDefault();
+      } else if (hashtags.length !== uniqueSet.size) {
+        input.setCustomValidity('Хештеги не должны повторятся');
+        input.classList.add('invalid');
+        evt.preventDefault();
+      } else {
+        input.setCustomValidity('');
+      }
+    });
+  }
+  input.reportValidity();
+};
+
+const getCustomValidation = (input) => {
+  if (input.value === '') {
+    input.setCustomValidity(validity.message);
+  } else if (isHashtagFirst(input) === false) {
+    input.setCustomValidity('Начините хэштег с решетки!');
+  } else if (valueLength(input) < MIN_HASHTAG_LENGTH) {
+    input.setCustomValidity(`Еще ${MIN_HASHTAG_LENGTH - valueLength(input)} симв.`);
+  } else if (valueLength(input) > MAX_HASHTAG_LENGTH) {
+    input.setCustomValidity(`Удалите еще ${valueLength(input) - MAX_HASHTAG_LENGTH} симв.`);
+  } else if (isSpaceAfterHash) {
+    pushUniqueHashtags(input);
+    input.setCustomValidity('Введите следующий хэштег после пробела. Максимум 5');
+  } else if (isHashAfterSpace) {
+    input.setCustomValidity('Пока не понятно');
+  } else {
+    input.setCustomValidity('');
+  }
+  input.reportValidity();
+};
 /* ------------------------------------------------------------------------------------------ */
 // TODO:Проверка наличия хештега
 inputTextHashtags.addEventListener(
   'input',
   () => {
-    const valueLength = inputTextHashtags.value.length;
-    const isHashtagFirst = inputTextHashtags.value.substr(0, 1) === '#';
-    if (isHashtagFirst) {
-      inputTextHashtags.setCustomValidity('Начните вводить хэштег со знака решетка!');
-    } else if (inputTextHashtags.value.length >= 1) { // Вернуть истину для регВыражения
-      fillInputArr(inputTextHashtags);
-    } else if (fillInputArr(inputTextHashtags) === false) {
-      console.log('false');
-    } else if (valueLength < MIN_HASHTAG_LENGTH) {
-      inputTextHashtags.setCustomValidity(`Еще ${MIN_HASHTAG_LENGTH - valueLength} симв.`);
-    } else if (valueLength > MAX_HASHTAG_LENGTH) {
-      inputTextHashtags.setCustomValidity(`Удалите еще ${valueLength - MAX_HASHTAG_LENGTH} симв.`);
-    } else {
-      inputTextHashtags.setCustomValidity('');
-    }
-    inputTextHashtags.reportValidity();
+    getCustomValidation(inputTextHashtags);
+    // validateHashtag(inputTextHashtags);
   },
 );
 
@@ -98,40 +124,57 @@ inputTextHashtags.addEventListener(
 //   }
 // }
 
-// var str = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя';
-// var regexp = /[А-Д]/gi;
-// var matches_array = str.match(regexp);
-// console.log(matches_array);
+// const MAX_HASHTAGS = 5;
+// const REG_HASHTAG = /^#[A-za-zА-Яа-я0-9]{1,19}$/;
+// const INVALID_INPUT_STYLE = 'red';
+// const VALID_INPUT_STYLE = 'green';
+// const TEXT_VALIDATE = `Хэш-тег должен начинаться с символа # и состоять из букв и чисел.
+// Максимальная длина одного хэш-тега 20 символов, включая символ #.
+// Хэш-теги должны разделяться пробелами`;
+// const ERROR_NO_REPEAT = 'Хэштеги не должны повторяться';
+// const MAX_COMMENTS_SYMBOL = 140;
+// const textHashtags = document.querySelector('.text__hashtags');
+// const textDescription = document.querySelector('.text__description');
 
-// RegEx
-// \D - числа [^0-9]
-// [\s] - whitespace
-// [[:punct:]] - punctuation
-// [\W] symbols
-// [pattern, flags] flag - i => игнорирование регистра
-
-// new RegExp('', 'i')
-// const objPhrases = {
-//   firstLetterIsHash: 'Хеш-тег начинается с символа \'#\' (решётка)',
-//   specSymbols: 'Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т.п.), cимволы пунктуации (тире, дефис запятая и т.п.), эмодзи и т.д.',
-//   singleHash: 'Хеш-тег не может состоять только из одной решётки',
-//   maxLength: 'Максимальная длина одного хэш-тега 20 символов, включая решётку',
-//   register: 'Хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом',
-//   spaceValidation: 'Хэш-теги разделяются пробелами',
-//   unique: 'Один и тот же хэш-тег не может быть использован дважды',
-//   quantity: 'нельзя указать больше пяти хэш-тегов',
-//   required: 'хэш-теги необязательны',
-//   isEsc: 'Если фокус находится в поле ввода хэш-тега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения',
+// const validationFormHashtag = (evt) => {
+//   if (textHashtags.value !== '') {
+//     const hashtags = textHashtags.value.toLowerCase().trim().split(' ').filter((hashtag) => hashtag);
+//     const hashtagsSet = new Set(hashtags);
+//     hashtags.forEach((hashtag) => {
+//       if (!REG_HASHTAG.test(hashtag)) {
+//         textHashtags.setCustomValidity(TEXT_VALIDATE);
+//         textHashtags.style.outlineColor = INVALID_INPUT_STYLE;
+//         evt.preventDefault();
+//       } else if (hashtags.length !== hashtagsSet.size) {
+//         textHashtags.setCustomValidity(ERROR_NO_REPEAT);
+//         textHashtags.style.outlineColor = INVALID_INPUT_STYLE;
+//         evt.preventDefault();
+//       } else {
+//         textHashtags.setCustomValidity('');
+//         textHashtags.style.outlineColor = VALID_INPUT_STYLE;
+//       }
+//       textHashtags.reportValidity();
+//     });
+//     if (hashtags.length > MAX_HASHTAGS) {
+//       textHashtags.setCustomValidity(`Количество хэштегов должно быть не более ${MAX_HASHTAGS}`);
+//     }
+//   } else {
+//     textHashtags.setCustomValidity('');
+//     textHashtags.style.outlineColor = VALID_INPUT_STYLE;
+//   }
 // };
-// Комментарий
+// textHashtags.addEventListener('input', validationFormHashtag);
 
-// 7. TODO:Реализуйте логику проверки так, чтобы, как минимум, она срабатывала при попытке отправить форму
-// и не давала этого сделать, если форма заполнена не по правилам. При желании, реализуйте проверки
-//  сразу при вводе значения в поле.
-
-// Как отменить обработчик Esc при фокусе?
-// Задача не имеет одного верного решения, однако намекнём на самый простой — использовать stopPropagation
-// для события нажатия клавиш в поле при фокусе
-
-// Валидация хеш-тегов?
-// Поля, не перечисленные в техзадании, но существующие в разметке, особой валидации не требуют.
+// const validationFormDescription = (evt) => {
+//   const textDescriptionLength = checkStringLength(textDescription.value, MAX_COMMENTS_SYMBOL);
+//   if (!textDescriptionLength) {
+//     textDescription.setCustomValidity(`Удалите лишние ${textDescription.value.length - MAX_COMMENTS_SYMBOL} симв.`);
+//     textDescription.style.outlineColor = INVALID_INPUT_STYLE;
+//     evt.preventDefault();
+//   } else {
+//     textDescription.setCustomValidity('');
+//     textDescription.style.outlineColor = VALID_INPUT_STYLE;
+//   }
+//   textDescription.reportValidity();
+// };
+// textDescription.addEventListener('input', validationFormDescription);
