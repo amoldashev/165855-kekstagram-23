a96128200c9eb5b92f840baf2ea147e90f9b8f31
Azamat M <amoldashev@gmail.com>
2021-07-07 12:54:32 +0500

    Правит валидацию и исправляет условия валидации

--- js/validation.js
+++ js/validation.js
@@ -1,17 +1,16 @@
 import {getValueLength, isEscEvent} from './utils.js';
 import {onPopupEscKeydown} from './image-upload-form.js';
 
-// Валидация поля хэш-тег
 const inputTextHashtags = document.querySelector('.text__hashtags');
 const form = document.querySelector('.img-upload__form');
+const inputTextDescription = document.querySelector('.text__description');
 
+const MAX_SYMBOL_LENGTH = 140;
 const MIN_HASHTAG_LENGTH = 2;
 const MAX_HASHTAG_LENGTH = 20;
 
 const SingleHashExp = /^#[A-Za-zА-Яа-яЁё0-9]*$/;
-
 const whiteSpace = /\s/;
-
 const isHashtagFirst = (input) => input.value.substr(0, 1) === '#';
 
 const validateAllHashtags = (input) => {
@@ -20,15 +19,14 @@ const validateAllHashtags = (input) => {
     if (!SingleHashExp.test(hash)) {
       input.setCustomValidity('Cтрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы, символы пунктуации и эмодзи');
     } else if (hashtags.length !== [...new Set(hashtags)].length) {
-      input.setCustomValidity('Хештеги не должны повторятся');
+      input.setCustomValidity('Хеш-теги не должны повторятся');
     } else if (hashtags.length > 5) {
-      input.setCustomValidity('5 хэштегов - максимум!');
+      input.setCustomValidity('5 хэш-тегов - максимум!');
     } else if (hash.length > MAX_HASHTAG_LENGTH) {
       input.setCustomValidity(`Удалите ещё ${hash.length - MAX_HASHTAG_LENGTH} симв.`);
     } else {
-      input.setCustomValidity('');
+      return hashtags;
     }
-    input.reportValidity();
   });
 };
 
@@ -37,8 +35,8 @@ const validateInputHashtag = (input) => {
     input.setCustomValidity('Начините хэштег с решетки!');
   } else if (getValueLength(input) < MIN_HASHTAG_LENGTH) {
     input.setCustomValidity(`Минимум ещё ${MIN_HASHTAG_LENGTH - getValueLength(input)} симв.`);
-  } else if (whiteSpace.test(input.value)) {
-    input.setCustomValidity('Введите сл. хэш-тег после пробела');
+  } else if (!whiteSpace.test(input.value) === false) {
+    input.setCustomValidity('Вводите хэш-теги, разделяя каждый хеш-тег пробелом');
     validateAllHashtags(input);
   } else {
     input.setCustomValidity('');
@@ -46,19 +44,12 @@ const validateInputHashtag = (input) => {
   input.reportValidity();
 };
 
-// Добавляет событие для поля хэштег
 inputTextHashtags.addEventListener(
   'input',
   () => {
     validateInputHashtag(inputTextHashtags);
   },
 );
-
-inputTextHashtags.removeEventListener('input', validateInputHashtag);
-
-// Валидация поля комментарий
-const inputTextDescription = document.querySelector('.text__description');
-const MAX_SYMBOL_LENGTH = 140;
 
 const validateComments = (input) => {
   if (getValueLength(input) < MAX_SYMBOL_LENGTH) {
@@ -71,7 +62,6 @@ const validateComments = (input) => {
   input.reportValidity();
 };
 
-// Добавляет событие для поля комментарий
 inputTextDescription.addEventListener(
   'input',
   () => {
@@ -79,23 +69,18 @@ inputTextDescription.addEventListener(
   },
 );
 
-inputTextDescription.removeEventListener('input', validateComments);
-
-// Добавляет событие Esc узлу window
 window.addEventListener('keydown', onPopupEscKeydown);
 
-// Делегирует форме улавливать событие на инпуте
-const onFormClick = (evt) => {
-  if (evt.target.nodeName === 'INPUT') {
-    evt.stopPropagation();
-  }
+const FormClickHandler = (evt) => {
+  evt.stopPropagation();
 };
 
-// Добавляет форме событие по нажатию Esc
 form.addEventListener(
   'keydown',
   (evt) => {
     if (isEscEvent) {
-      onFormClick(evt);
+      FormClickHandler(evt);
     }
   });
+
+inputTextHashtags.removeEventListener('input', validateInputHashtag);
