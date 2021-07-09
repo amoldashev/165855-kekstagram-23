5d586c038e784347ca6f59a484bf3ebc5d2af2d7
Azamat M <amoldashev@gmail.com>
2021-07-09 12:48:04 +0500

    Вносит модуль эффекты

---
+++ js/effects.js
@@ -0,0 +1,7 @@
+import '../nouislider/nouislider.js';
+
+const sliderElement = document.querySelector('.effect-level__slider');
+
+noUiSlider.create(
+  sliderElement, {}
+)
--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ import './previews.js';
 import './image-upload-form.js';
 import './validation.js';
 import './scale.js';
+import './effects.js';
 
 setPhotocard(MAX_PHOTOCARD_AMOUNT);
