12fbd951a40d0bf80deba69234ce57a171093a4e
Azamat M <aaamoldashev@gmail.com>
2022-06-22 14:47:03 +0500

    Предаврительно, удаляет лишний код

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 import './big-picture.js';
 import './previews.js';
-import { setPhotocard } from './set-photocard.js';
-import { MAX_PHOTOCARD_AMOUNT } from './data.js';
+import './fetch.js';
 import './create-comment.js';
 import './image-upload-form.js';
 import './validation.js';
-
-setPhotocard(MAX_PHOTOCARD_AMOUNT);
-
-// const dataArray = new Array();
-// fetch(dataUrl)
-//   .then((blob) => blob.json())
-//   .then(data => dataArray.push(...data));
-//   // .then((data) => dataArray.push(...data));
-// getPreviews(dataArray);
