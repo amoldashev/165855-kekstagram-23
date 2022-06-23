403a27ca75eb0f9db458edfdfd25b45f38595038
Azamat M <aaamoldashev@gmail.com>
2022-06-23 13:06:15 +0500

    Завершает задания данного модуля

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,8 +7,11 @@ const cancelButton = body.querySelector('.big-picture__cancel');
 const likesCount = body.querySelector('.likes-count');
 const commentsCount = document.querySelector('.comments-count');
 const descriptionEl = document.querySelector('.social__caption');
+const counter = document.querySelector('.social__comment-count'); //FIXME: del later on
+const loader = document.querySelector('.comments-loader'); //FIXME: del later on
 
-function hideModal() { // FIXME: установить прежние данные
+
+function hideModal() {
   body.classList.remove('modal-open');
   modal.classList.add('hidden');
   // eslint-disable-next-line no-use-before-define
@@ -29,11 +32,13 @@ function setBigPicture({url, likes, comments, description}) {
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
   descriptionEl.textContent = description;
+  counter.classList.add('hidden'); // FIXME: del later on
+  loader.classList.add('hidden'); // FIXME: del later on
 }
 
 const previewClickHandler = (evt, photo) => {
   evt.preventDefault();
-  modal.classList.add('modal-open');
+  body.classList.add('modal-open');
   modal.classList.remove('hidden');
   document.addEventListener('keydown', onEsc, { 'once': true });
   cancelButton.addEventListener('click', handleButton, { 'once': true });
