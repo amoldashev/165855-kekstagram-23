8d15857b80d1c2557ab1abb96c59b478b23cc0ca
Azamat M <aaamoldashev@gmail.com>
2022-06-23 12:57:56 +0500

    Соединяет модули превью и открытия большого онка

--- js/previews.js
+++ js/previews.js
@@ -1,4 +1,6 @@
-const pictureTemplate = document.querySelector('#picture').content;
+import { previewClickHandler } from './big-picture.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const gallerySection = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
@@ -7,12 +9,13 @@ const setPreviewTemplate = (template, {url, likes, comments}) => {
   template.querySelector('.picture__likes').textContent = likes;
   template.querySelector('.picture__comments').textContent = comments.length;
   return template;
-}
+};
 
 function getPreviews(photos) {
   photos.forEach((photo) => {
-    const element = setPreviewTemplate(pictureTemplate.cloneNode(true), photo)
+    const element = setPreviewTemplate(pictureTemplate.cloneNode(true), photo);
     fragment.appendChild(element);
+    element.addEventListener('click', (evt) => previewClickHandler(evt, photo));
   });
   gallerySection.appendChild(fragment);
 }
