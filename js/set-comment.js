2f19e13976676824fda397df437e56092d8a11f8
Azamat M <aaamoldashev@gmail.com>
2022-06-24 16:01:33 +0500

    Исправляет ошибки линтером

--- js/set-comment.js
+++ js/set-comment.js
@@ -1,13 +1,7 @@
 const commentList = document.querySelector('.social__comments');
 const commentElements = document.querySelectorAll('.social__comment');
 const commentCounter = document.querySelector('.social__comment-count');
-const commenstLoader = document.querySelector('.comments-loader');
 const MAX_LENGTH = 5;
-
-const showComments = (comments) => {
-  commentCounter.textContent = `${comments.length < MAX_LENGTH ? comments.length  : MAX_LENGTH} из ${comments.length} комментариев`;
-  comments.slice(0, 5).forEach((comment) => setCommentTemplate(commentElements[0].cloneNode(true), comment));
-}
 
 const setCommentTemplate = (template, { avatar, message, name }) => {
   const avatarElement = template.querySelector('.social__picture');
@@ -17,15 +11,20 @@ const setCommentTemplate = (template, { avatar, message, name }) => {
   avatarElement.alt = name;
   const fragment = document.createDocumentFragment();
   fragment.append(template);
-  commentList.appendChild(template)
+  commentList.appendChild(template);
   return template;
+};
+
+const showComments = (comments) => {
+  commentCounter.textContent = `${comments.length < MAX_LENGTH ? comments.length  : MAX_LENGTH} из ${comments.length} комментариев`;
+  comments.slice(0, 5).forEach((comment) => setCommentTemplate(commentElements[0].cloneNode(true), comment));
 };
 
 const cleanCommentList = (list) => list.innerHTML = '';
 
 function handleComments(comments) {
   cleanCommentList(commentList);
-  showComments(comments)
+  showComments(comments);
 }
 
 export { handleComments };
