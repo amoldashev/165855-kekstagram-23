558abeac4fffe470cc9491bde5211ecf424ba092
verminCat <amoldashev@gmail.com>
2022-06-28 06:12:43 +0500

    Показывает 5 комментариев

--- js/set-comment.js
+++ js/set-comment.js
@@ -3,11 +3,11 @@ const commentElements = document.querySelectorAll('.social__comment');
 const commentCounter = document.querySelector('.social__comment-count');
 const commenstLoader = document.querySelector('.comments-loader');
 const MAX_LENGTH = 5;
+const LOAD_STEP = 5;
 
-const showComments = (comments) => {
-  commentCounter.textContent = `${comments.length < MAX_LENGTH ? comments.length  : MAX_LENGTH} из ${comments.length} комментариев`;
-  comments.slice(0, 5).forEach((comment) => setCommentTemplate(commentElements[0].cloneNode(true), comment));
-}
+const countComments = (arr) => {
+  commentCounter.innerHTML = `${arr.length < MAX_LENGTH ? arr.length  : MAX_LENGTH} из ${arr.length} комментариев`;
+};
 
 const setCommentTemplate = (template, { avatar, message, name }) => {
   const avatarElement = template.querySelector('.social__picture');
@@ -17,15 +17,22 @@ const setCommentTemplate = (template, { avatar, message, name }) => {
   avatarElement.alt = name;
   const fragment = document.createDocumentFragment();
   fragment.append(template);
-  commentList.appendChild(template)
+  commentList.appendChild(template);
   return template;
+};
+
+const showComments = (comments, step) => {
+  countComments(comments);
+  const currentComments = comments.slice(0, step);
+  currentComments.forEach((item) => setCommentTemplate(commentElements[0].cloneNode(true), item));
 };
 
 const cleanCommentList = (list) => list.innerHTML = '';
 
 function handleComments(comments) {
   cleanCommentList(commentList);
-  showComments(comments)
+  const currentStep = LOAD_STEP;
+  commenstLoader.addEventListener('click', showComments(comments, currentStep));
 }
 
 export { handleComments };
