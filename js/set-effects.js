62a682ca27b43bb641a0e2f69f72f37e607c28da
Azamat M <amoldashev@gmail.com>
2022-10-05 06:50:51 +0500

    Вносит правки

--- js/set-effects.js
+++ js/set-effects.js
@@ -1,39 +1,112 @@
 const preview = document.querySelector('.img-upload__preview');
 const effects = document.querySelectorAll('.effects__radio');
+const container = document.querySelector('.effect-level__slider');
+container.setAttribute('id', 'slider');
+const slider = document.getElementById('slider');
 
-// const EFFECTS = [
-//   "none",
-//   "chrome",
-//   "sepia",
-//   "marvin",
-//   "phobos",
-//   "heat",
-// ]
+const sliderHandler = () => {
+  noUiSlider.create(slider, {
+    start: [100],
+    step: 10,
+    connect: true,
+    range: {
+      min: 0,
+      max: 100,
+    },
+  });
 
-var slider = document.getElementById('slider');
+  slider.classList.add('hidden');
+};
 
-noUiSlider.create(slider, {
-  start: [0],
-  step: 10,
-  connect: true,
-  range: {
-      'min': 0,
-      'max': 100
+window.addEventListener('DOMContentLoaded', sliderHandler);
+
+const Effects = {
+  default: {
+    filter: 'unset',
+    step: 0,
+    max: 100,
+    postfix: '',
+  },
+  chrome: {
+    filter: 'grayscale',
+    step: 0.1,
+    max: 1,
+    postfix: '',
+  },
+  sepia: {
+    filter: 'sepia',
+    step: 0.1,
+    max: 1,
+    postfix: '',
+  },
+  marvin: {
+    filter: 'invert',
+    step: 1,
+    max: 100,
+    postfix: '%',
+  },
+  phobos: {
+    filter: 'blur',
+    step: 0.1,
+    max: 3,
+    postfix: 'px',
+  },
+  heat: {
+    filter: 'brightness',
+    step: 0.1,
+    max: 3,
+    postfix: '',
+  },
+};
+
+const setEffect = (targetValue) => {
+  // let hiddenField = document.querySelector('.effect-level__value').value;
+
+  if (targetValue === 'none') {
+    slider.noUiSlider.off();
+    slider.noUiSlider.reset();
+    preview.style.filter = '';
+  } else {
+    slider.noUiSlider.updateOptions({
+      step: Effects[`${targetValue}`].step,
+      connect: true,
+      range: {
+        min: 0,
+        max: Effects[`${targetValue}`].max,
+      },
+    });
+
+    slider.noUiSlider.on('slide', () => {
+      slider.noUiSlider.updateOptions({
+        step: Effects[`${targetValue}`].step,
+        connect: true,
+        range: {
+          min: 0,
+          max: Effects[`${targetValue}`].max,
+        },
+      });
+      preview.style.filter = `${Effects[`${targetValue}`].filter}(${Math.ceil(
+        slider.noUiSlider.get(),
+      )}${Effects[`${targetValue}`].postfix})`;
+    });
   }
-});
+};
 
 // обработчик
 const effectHandler = (evt) => {
-  preview.className = '';
-  slider.removeAttribute('disabled');
-
-  if (evt.target.value == 'none') {
-    preview.className = `img-upload__preview effect__radio--${evt.target.value}`;
+  if (evt.target.value === 'none') {
+    setEffect(evt.target.value);
+    preview.className = `img-upload__preview effects__preview--${evt.target.value}`;
+    slider.classList.add('hidden');
     slider.setAttribute('disabled', true);
-  } return preview.className = `img-upload__preview effect__radio--${evt.target.value}`;
-}
-effects.forEach((effect) => effect.addEventListener('click', effectHandler))
+  } else {
+    setEffect(evt.target.value);
+    slider.classList.remove('hidden');
+    slider.removeAttribute('disabled');
+    slider.noUiSlider.reset();
+    preview.className = '';
+    return (preview.className = `img-upload__preview effects__preview--${evt.target.value}`);
+  }
+};
 
-// уровень эффекта записывается в поле .effect-level__value
-// slider.setAttribute('disabled', true);
-// slider.removeAttribute('disabled');
+effects.forEach((effect) => effect.addEventListener('click', effectHandler));
